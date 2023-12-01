c8fc1e74e7652275d171f2854da77f36d850a7d6
Enifome Alalor <fomealalor@gmail.com>
2023-12-01 11:05:54 +0100

    Multiple tasks working, only for current day

--- src/App.js
+++ src/App.js
@@ -2,29 +2,33 @@ import './App.css';
 import { useSession, useSupabaseClient, useSessionContext } from '@supabase/auth-helpers-react';
 import DateTimePicker from 'react-datetime-picker';
 import { useState } from 'react';
+import 'react-datetime-picker/dist/DateTimePicker.css';
+import 'react-calendar/dist/Calendar.css';
+import 'react-clock/dist/Clock.css';
 
 function App() {
-  const [ start, setStart ] = useState(new Date());
-  const [ end, setEnd ] = useState(new Date());
-  const [ eventName, setEventName ] = useState("");
-  const [ eventDescription, setEventDescription ] = useState("");
+  const [start, setStart] = useState(new Date());
+  const [end, setEnd] = useState(new Date());
+  const [eventName, setEventName] = useState("");
+  const [eventDescription, setEventDescription] = useState("");
+  const [tasks, setTasks] = useState([]); // Maintain a list of tasks
 
   const session = useSession(); // tokens, when session exists we have a user
   const supabase = useSupabaseClient(); // talk to supabase!
   const { isLoading } = useSessionContext();
-  
-  if(isLoading) {
-    return <></>
+
+  if (isLoading) {
+    return <></>;
   }
 
   async function googleSignIn() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        scopes: 'https://www.googleapis.com/auth/calendar'
-      }
+        scopes: 'https://www.googleapis.com/auth/calendar',
+      },
     });
-    if(error) {
+    if (error) {
       alert("Error logging in to Google provider with Supabase");
       console.log(error);
     }
@@ -34,41 +38,86 @@ function App() {
     await supabase.auth.signOut();
   }
 
-  async function createCalendarEvent() {
-    console.log("Creating calendar event");
-    const event = {
-      'summary': eventName,
-      'description': eventDescription,
-      'start': {
-        'dateTime': start.toISOString(), // Date.toISOString() ->
-        'timeZone': Intl.DateTimeFormat().resolvedOptions().timeZone // America/Los_Angeles
-      },
-      'end': {
-        'dateTime': end.toISOString(), // Date.toISOString() ->
-        'timeZone': Intl.DateTimeFormat().resolvedOptions().timeZone // America/Los_Angeles
-      }
-    }
-    await fetch("https://www.googleapis.com/calendar/v3/calendars/primary/events", {
+// ... (previous code)
+
+async function createCalendarEvent(task) {
+  console.log("Creating calendar event");
+
+  // Automatically detect the user's time zone
+  const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+  const formatDate = (date) => {
+    return date.toISOString().replace(/(\.\d{3})\d+/, "$1"); // Remove milliseconds
+  };
+
+  const formatTime = (date) => {
+    return date.toLocaleTimeString('en-US', { hour12: true });
+  };
+
+  const event = {
+    'summary': task.eventName,
+    'description': task.eventDescription,
+    'start': {
+      'dateTime': formatDate(task.start),
+      'timeZone': userTimeZone,
+    },
+    'end': {
+      'dateTime': formatDate(task.end),
+      'timeZone': userTimeZone,
+    },
+  };
+
+  try {
+    const response = await fetch("https://www.googleapis.com/calendar/v3/calendars/primary/events", {
       method: "POST",
       headers: {
-        'Authorization':'Bearer ' + session.provider_token // Access token for google
+        'Authorization': 'Bearer ' + session.provider_token,
+        'Content-Type': 'application/json',
       },
-      body: JSON.stringify(event)
-    }).then((data) => {
-      return data.json();
-    }).then((data) => {
-      console.log(data);
-      alert("Event created, check your Google Calendar!");
+      body: JSON.stringify(event),
     });
-  }
 
-  console.log(session);
-  console.log(start);
-  console.log(eventName);
-  console.log(eventDescription);
+    if (!response.ok) {
+      throw new Error(`Error: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    console.log(data);
+    alert("Event created, check your Google Calendar!");
+  } catch (error) {
+    console.error(error);
+    alert("Error creating event. Please try again.");
+  }
+}
+
+// ... (remaining code)
+
+
+  const handleCreateTask = () => {
+    const newTask = {
+      start,
+      end,
+      eventName,
+      eventDescription,
+    };
+    setTasks([...tasks, newTask]); // Add the new task to the list
+    // Clear the input fields after creating a task
+    setStart(new Date());
+    setEnd(new Date());
+    setEventName("");
+    setEventDescription("");
+  };
+
+  const handlePushToCalendar = async () => {
+    for (const task of tasks) {
+      await createCalendarEvent(task); // Create events one at a time
+    }
+    setTasks([]); // Clear the tasks list after pushing to calendar
+  };
+
   return (
     <div className="App">
-      <div style={{width: "400px", margin: "30px auto"}}>
+      <div style={{ width: "400px", margin: "30px auto" }}>
         {session ?
           <>
             <h2>Hey there {session.user.email}</h2>
@@ -77,17 +126,23 @@ function App() {
             <p>End of your event</p>
             <DateTimePicker onChange={setEnd} value={end} />
             <p>Event name</p>
-            <input type="text" onChange={(e) => setEventName(e.target.value)} />
+            <input type="text" value={eventName} onChange={(e) => setEventName(e.target.value)} />
             <p>Event description</p>
-            <input type="text" onChange={(e) => setEventDescription(e.target.value)} />
+            <input type="text" value={eventDescription} onChange={(e) => setEventDescription(e.target.value)} />
             <hr />
-            <button onClick={() => createCalendarEvent()}>Create Calendar Event</button>
+            <button onClick={handleCreateTask}>Create Task</button>
+            <ul>
+              {tasks.map((task, index) => (
+                <li key={index}>{`${task.eventName} - ${task.start.toISOString()} to ${task.end.toISOString()}`}</li>
+              ))}
+            </ul>
+            <button onClick={handlePushToCalendar}>Push to Google Calendar</button>
             <p></p>
-            <button onClick={() => signOut()}>Sign Out</button>
+            <button onClick={signOut}>Sign Out</button>
           </>
           :
           <>
-            <button onClick={() => googleSignIn()}>Sign In With Google</button>
+            <button onClick={googleSignIn}>Sign In With Google</button>
           </>
         }
       </div>
