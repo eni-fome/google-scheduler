d202792614357fb4d868be2642d56f76132edf96
eni-fome <fomealalor@gmail.com>
2024-01-04 21:37:22 +0100

    task editing from app successful,edit desc issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import { useSession, useSupabaseClient, useSessionContext } from '@supabase/auth-helpers-react';
 import DateTimePicker from 'react-datetime-picker';
@@ -8,10 +8,12 @@ import 'react-calendar/dist/Calendar.css';
 import 'react-clock/dist/Clock.css';
 
 
+
 function App() {
   type RecurrenceType = 'daily' | 'weekly' | 'monthly' | 'none';
 
   interface Task {
+    id?: string;
     start: Date;
     end: Date;
     eventName: string;
@@ -32,14 +34,27 @@ function App() {
   const [end, setEnd] = useState<Date>(new Date());
   const [eventName, setEventName] = useState<string>("");
   const [eventDescription, setEventDescription] = useState<string>("");
-  const [tasks, setTasks] = useState<Task[]>([]); 
+  const [tasks, setTasks] = useState<Task[]>([]);
   const [recurrence, setRecurrence] = useState<RecurrenceType>("none");
+  const [showForm, setShowForm] = useState(true);
+  const [showTasksList, setShowTasksList] = useState(true);
+  const [updatedTask, setUpdatedTask] = useState<Task | null>(null);
+  const [editingEvent, setEditingEvent] = useState<Task | null>(null);
+
+
   
 
-  const session = useSession(); // tokens, when session exists we have a user
-  const supabase = useSupabaseClient(); // talk to supabase!
+  const session = useSession();
+  const supabase = useSupabaseClient(); 
   const { isLoading } = useSessionContext();
 
+
+  useEffect(() => {
+
+    if (session) {
+      fetchCalendarEvents();
+    }
+  }, [session])
 
   const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
   
@@ -57,6 +72,19 @@ function App() {
   const recurrenceRule = (recurrence: 'daily' | 'weekly' | 'monthly') =>
   `RRULE:FREQ=${recurrence.toUpperCase()}`;
 
+  const convertRecurrence = (recurrenceString: string[] | undefined): RecurrenceType => {
+    if (recurrenceString && recurrenceString[0].includes('DAILY')) {
+      return 'daily';
+    }
+    if (recurrenceString && recurrenceString[0].includes('WEEKLY')) {
+      return 'weekly';
+    }
+    if (recurrenceString && recurrenceString[0].includes('MONTHLY')) {
+      return 'monthly';
+    }
+    return 'none';
+  };
+  
 
   if (isLoading) {
     return <></>;
@@ -78,19 +106,12 @@ function App() {
   async function signOut() {
     await supabase.auth.signOut();
   }
-
-  const handleRemoveTask = (index: any) => {
-    const updatedTasks = [...tasks];
-    updatedTasks.splice(index, 1);
-    setTasks(updatedTasks);
-  };
-
   async function createCalendarEvent(task: Task) {
     console.log("Creating calendar event");
   
     const event: Event = {
       'summary': task.eventName,
-      'description': task.eventDescription,
+      'description': `${task.eventDescription} [Created with MyCalendarApp]`,
       'start': {
         'dateTime': task.start.toISOString(),
         'timeZone': userTimeZone,
@@ -128,18 +149,22 @@ function App() {
       console.error(error);
       alert("Error creating event. Please try again.");
     }
+    fetchCalendarEvents();
   }
 
-  const handleCreateTask = () => {
+
+  const handleCreateTask = async () => {
     const newTask = {
+
       start,
       end,
       eventName,
       eventDescription,
       recurrence,
     };
-
-    setTasks([...tasks, newTask]);
+  
+    await createCalendarEvent(newTask);
+  
     setStart(new Date());
     setEnd(new Date());
     setEventName("");
@@ -147,12 +172,189 @@ function App() {
     setRecurrence("none");
   };
 
-  const handlePushToCalendar = async () => {
-    for (const task of tasks) {
-      await createCalendarEvent(task); // Create events one at a time
+  const handleSaveTask = async () => {
+    const taskToSave = { start, end, eventName, eventDescription, recurrence };
+
+    if (updatedTask && updatedTask.id) {
+      // Update existing task
+      await editEvent(updatedTask.id, { ...updatedTask, ...taskToSave });
+    } else {
+      // Create new task
+      await createCalendarEvent(taskToSave);
     }
-    setTasks([]); // Clear the tasks list after pushing to calendar
+
+    // Reset form after save
+    setStart(new Date());
+    setEnd(new Date());
+    setEventName("");
+    setEventDescription("");
+    setRecurrence("none");
+    setUpdatedTask(null); // Reset updatedTask
   };
+  
+
+
+  async function fetchCalendarEvents() {
+    try {
+      const response = await fetch("https://www.googleapis.com/calendar/v3/calendars/primary/events", {
+        headers: {
+          'Authorization': 'Bearer ' + (session?.provider_token || ''),
+        },
+      });
+      
+      if (!response.ok) {
+        throw new Error(`Error: ${response.statusText}`);
+      }
+  
+      const data = await response.json();
+      const transformedTasks: Task[] = data.items
+      .map((item: any) => {
+        // Check if start and end dates are present
+        if (!item.start || !item.start.dateTime || !item.end || !item.end.dateTime) {
+          console.warn("Skipping an event with missing start/end dates:", item);
+          return null;  // Return null or some default value for missing dates
+        }
+    
+        // Check if the event was created with your app
+        if (!item.description?.includes("[Created with MyCalendarApp]")) {
+          return null;  // Skip events not created with your app
+        }
+    
+        return {
+          id: item.id,
+          start: new Date(item.start.dateTime),
+          end: new Date(item.end.dateTime),
+          eventName: item.summary,
+          eventDescription: item.description || '',
+          recurrence: convertRecurrence(item.recurrence)
+        };
+      })
+      .filter((task: Task | null) => task !== null); 
+      
+      
+  
+      setTasks(transformedTasks);
+    } catch (error) {
+      console.error(error);
+      // Handle errors
+    }
+  }
+
+
+  async function editEvent(eventId: string, task: Task) {
+    const event: Event = {
+      summary: task.eventName,
+      description: task.eventDescription,
+      start: { dateTime: task.start.toISOString(), timeZone: userTimeZone },
+      end: { dateTime: task.end.toISOString(), timeZone: userTimeZone }
+    }
+    
+    if (task.recurrence !== 'none') {
+      event.recurrence = [recurrenceRule(task.recurrence)];
+    } else {
+      event.recurrence = []; // Clear recurrence if it's set to 'none'
+    }
+  
+    try {
+      const response = await fetch(`https://www.googleapis.com/calendar/v3/calendars/primary/events/${eventId}`, {
+        method: 'PUT',
+        headers: {
+          'Authorization': `Bearer ${session?.provider_token || ''}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(event),
+      });
+  
+      if (!response.ok) {
+        throw new Error(`HTTP Error: ${response.status}`);
+    }
+
+    const jsonResponse = await response.json();
+    console.log('Update successful:', jsonResponse);
+} catch (error) {
+    console.error('Error during event update:', error);
+    // Optionally log the entire request details for debugging
+    console.log('Request details:', eventId, updatedTask);
+}
+fetchCalendarEvents()
+  }
+
+  const startEditingEvent = (task: Task) => {
+    setUpdatedTask(task); // Use updatedTask for editing
+    setStart(task.start);
+    setEnd(task.end);
+    setEventName(task.eventName);
+    setEventDescription(task.eventDescription);
+    setRecurrence(task.recurrence);
+  };
+
+  const handleEditTask = async (taskId: string | undefined) => {
+    // Find the task in the tasks array
+    const taskIndex = tasks.findIndex((task) => task.id === taskId);
+  
+    if (taskIndex !== -1) {
+      const updatedTask = {
+        ...tasks[taskIndex],
+        eventName,
+        eventDescription,
+        start,
+        end,
+        recurrence
+      };
+  
+      // Update Google Calendar Event
+      await editEvent(taskId as string, updatedTask);
+  
+      // Update Local State
+      const newTasks = [...tasks];
+      newTasks[taskIndex] = updatedTask;
+  
+      setTasks(newTasks);
+  
+      // Clear Form
+      setEventName('');
+      setEventDescription('');
+      setStart(new Date());
+      setEnd(new Date());
+      setRecurrence('none');
+      setUpdatedTask(null); // Reset updatedTask
+    }
+  };
+  
+  
+  
+  
+
+  
+  async function deleteEvent(eventId: string) {
+    try {
+      const response = await fetch(`https://www.googleapis.com/calendar/v3/calendars/primary/events/${eventId}`, {
+        method: 'DELETE',
+        headers: {
+          'Authorization': `Bearer ${session?.provider_token || ''}`,
+        },
+      });
+  
+      if (!response.ok) {
+        throw new Error(`Error: ${response.statusText}`);
+      }
+  
+      // Handle successful deletion
+    } catch (error) {
+      console.error(error);
+      // Handle errors
+    }
+    fetchCalendarEvents()
+  }
+
+  
+  
+  // const handleRemoveTask = (index: any) => {
+  //   const updatedTasks = [...tasks];
+  //   updatedTasks.splice(index, 1);
+  //   setTasks(updatedTasks);
+  // };
+
 
   return (
     <div className="App">
@@ -160,38 +362,62 @@ function App() {
         {session ? (
           <>
             <h2>Hey there {session.user.email}</h2>
-            <p>Start of your event</p>
-            <DateTimePicker onChange={(date) => setStart(date || new Date())} value={start} />
-            <p>End of your event</p>
-            <DateTimePicker onChange={(date) => setEnd(date || new Date())} value={end} />
-            <p>Event name</p>
-            <input type="text" value={eventName} onChange={(e) => setEventName(e.target.value)} />
-            <p>Event description</p>
-            <input type="text" value={eventDescription} onChange={(e) => setEventDescription(e.target.value)} />
-            <p>Recurrence</p>
-            <select value={recurrence} onChange={(e) => setRecurrence(e.target.value as RecurrenceType)}>
-              <option value="none">None</option>
-              <option value="daily">Daily</option>
-              <option value="weekly">Weekly</option>
-              <option value="monthly">Monthly</option>
-            </select>
-            <hr />
-            <button onClick={handleCreateTask}>Create Task</button>
-            <ul>
-              {tasks.map((task, index) => (
-              <li key={index}>
-              <div>
-                {`${task.eventName} - From ${formatDate(task.start)} to ${formatDate(task.end)}`}
-              </div>
-              <div>Recurrence: {task.recurrence}</div>
-              <button onClick={() => handleRemoveTask(index)} style={{border: 'none', background: 'none'}}>
-                  <span role="img" aria-label="Remove Task">❌</span>
-                </button>
-              </li>
-              ))}
-    </ul>
-            <button onClick={handlePushToCalendar}>Push to Google Calendar</button>
-            <p></p>
+            <button onClick={() => setShowForm(!showForm)}>
+              {showForm ? "Close" : "Add Task"}
+            </button>
+            {showForm && (
+              <>
+                <p>Start of your event</p>
+                <DateTimePicker onChange={(date) => setStart(date || new Date())} value={start} />
+                <p>End of your event</p>
+                <DateTimePicker onChange={(date) => setEnd(date || new Date())} value={end} />
+                <p>Event name</p>
+                <input 
+                  type="text" 
+                  value={eventName} 
+                  onChange={(e) => setEventName(e.target.value)} 
+                />
+                <p>Event description</p>
+                <input 
+                  type="text" 
+                  value={eventDescription} 
+                  onChange={(e) => setEventDescription(e.target.value)} 
+                />
+                <p>Recurrence</p>
+                <select 
+                  value={updatedTask ? updatedTask.recurrence : recurrence} 
+                  onChange={(e) => setRecurrence(e.target.value as RecurrenceType)}
+                >
+                  <option value="none">None</option>
+                  <option value="daily">Daily</option>
+                  <option value="weekly">Weekly</option>
+                  <option value="monthly">Monthly</option>
+                </select>
+                <hr />
+                {updatedTask ? (
+                  <button onClick={() => handleEditTask(updatedTask.id)}>Update Task</button>
+                ) : (
+                  <button onClick={handleCreateTask}>Create Task</button>
+                )}
+              </>
+            )}
+  
+            <button onClick={() => setShowTasksList(!showTasksList)}>
+              {showTasksList ? "Hide Tasks List" : "Show Tasks List"}
+            </button>
+            {showTasksList && (
+              <ul>
+                {tasks.map((task) => (
+                  <li key={task.id}>
+                    {`${task.eventName} - From ${formatDate(task.start)} to ${formatDate(task.end)}`}
+                    <div>Recurrence: {task.recurrence}</div>
+                    <button onClick={() => startEditingEvent(task)}>📝</button>
+                    <button onClick={() => task.id && deleteEvent(task.id)}>❌</button>
+                  </li>
+                ))}
+              </ul>
+            )}
+  
             <button className="signout" onClick={signOut}>
               Sign Out
             </button>
@@ -204,6 +430,5 @@ function App() {
       </div>
     </div>
   );
-}
-
-export default App;
+        }
+  export default App;
