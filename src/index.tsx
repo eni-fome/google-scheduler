84921cad6362b4185262bdac0e87c2d94f32ad07
Enifome Alalor <fomealalor@gmail.com>
2024-01-10 15:01:02 +0100

    Added .env

--- src/index.tsx
+++ src/index.tsx
@@ -5,27 +5,30 @@ import App from './App';
 import { createClient } from '@supabase/supabase-js';
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
 
-const supabase = createClient(
-  "https://xypexkoiuotgjzdsxxuz.supabase.co", 
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh5cGV4a29pdW90Z2p6ZHN4eHV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA2NDI4NzEsImV4cCI6MjAxNjIxODg3MX0.w_iyp-Go467sW4WSGw5VL83HRtiytmWMIwiIzbYXyLA"
-)
+const SUPABASE_CLIENT = process.env.REACT_APP_SUPABASE_CLIENT;
+const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
+
+if (!SUPABASE_CLIENT || !SUPABASE_URL) {
+   throw new Error('Supabase environment variables not found');
+}
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_CLIENT);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+   document.getElementById('root') as HTMLElement,
 );
 root.render(
-  <React.StrictMode>
-    <SessionContextProvider supabaseClient={supabase}>
-    <App />
-    </SessionContextProvider>
-  </React.StrictMode>
+   <React.StrictMode>
+      <SessionContextProvider supabaseClient={supabase}>
+         <App />
+      </SessionContextProvider>
+   </React.StrictMode>,
 );
-
 
 // reportWebVitals.ts
 const reportWebVitals = (onPerfEntry?: any) => {
-  // Dummy implementation, you can leave it empty or log something to the console
-  console.log("Web Vitals Report:", onPerfEntry);
+   // Dummy implementation, you can leave it empty or log something to the console
+   console.log('Web Vitals Report:', onPerfEntry);
 };
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
