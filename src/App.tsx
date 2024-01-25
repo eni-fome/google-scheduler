21e291104976af0302afbb1abda00ad8479b2556
Enifome Alalor <fomealalor@gmail.com>
2024-01-25 20:39:16 +0100

    removed commented out code

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,6 @@ function App() {
     const [showForm, setShowForm] = useState(true);
     const [showTasksList, setShowTasksList] = useState(true);
     const [updatedTask, setUpdatedTask] = useState<Task | null>(null);
-    // const [editingEvent, setEditingEvent] = useState<Task | null>(null);
 
     const session = useSession();
     const supabase = useSupabaseClient();
@@ -36,37 +35,6 @@ function App() {
             fetchCalendarEvents();
         }
     }, [session]);
-
-    // const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-
-    // const formatDate = (date: any) => {
-    //     return new Intl.DateTimeFormat('en-US', {
-    //         year: 'numeric',
-    //         month: 'long',
-    //         day: 'numeric',
-    //         hour: '2-digit',
-    //         minute: '2-digit',
-    //         timeZone: userTimeZone,
-    //     }).format(date);
-    // };
-
-    // const recurrenceRule = (recurrence: 'daily' | 'weekly' | 'monthly') =>
-    //     `RRULE:FREQ=${recurrence.toUpperCase()}`;
-
-    // const convertRecurrence = (
-    //     recurrenceString: string[] | undefined,
-    // ): RecurrenceType => {
-    //     if (recurrenceString && recurrenceString[0].includes('DAILY')) {
-    //         return 'daily';
-    //     }
-    //     if (recurrenceString && recurrenceString[0].includes('WEEKLY')) {
-    //         return 'weekly';
-    //     }
-    //     if (recurrenceString && recurrenceString[0].includes('MONTHLY')) {
-    //         return 'monthly';
-    //     }
-    //     return 'none';
-    // };
 
     if (isLoading) {
         return <></>;
