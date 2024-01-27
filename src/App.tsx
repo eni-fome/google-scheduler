31ffcd2498f1c8d5fa83b84ed5406c373f97f8a2
Enifome Alalor <fomealalor@gmail.com>
2024-01-27 04:30:46 +0100

    Removed unused code

--- src/App.tsx
+++ src/App.tsx
@@ -61,53 +61,6 @@ function App() {
     }
 
 
-    // async function createCalendarEvent(task: Task) {
-    //     console.log('Creating calendar event');
-
-    //     const event: Event = {
-    //         summary: task.eventName,
-    //         description: `${task.eventDescription} [Created with MyCalendarApp]`,
-    //         start: {
-    //             dateTime: task.start.toISOString(),
-    //             timeZone: userTimeZone,
-    //         },
-    //         end: {
-    //             dateTime: task.end.toISOString(),
-    //             timeZone: userTimeZone,
-    //         },
-    //     };
-
-    //     if (task.recurrence !== 'none') {
-    //         event.recurrence = [recurrenceRule(task.recurrence)];
-    //     }
-
-    //     try {
-    //         const response = await fetch(
-    //             apiUrl,
-    //             {
-    //                 method: 'POST',
-    //                 headers: {
-    //                     Authorization:
-    //                         'Bearer ' + (session?.provider_token || ''),
-    //                     'Content-Type': 'application/json',
-    //                 },
-    //                 body: JSON.stringify(event),
-    //             },
-    //         );
-
-    //         if (!response.ok) {
-    //             throw new Error(`Error: ${response.statusText}`);
-    //         }
-
-    //         const data = await response.json();
-    //         console.log(data);
-    //         alert('Event created, check your Google Calendar!');
-    //     } catch (error) {
-    //         console.error(error);
-    //         alert('Error creating event. Please try again.');
-    //     }
-    //     fetchCalendarEvents();
-    // }
 
     const handleCreateTask = async () => {
         const newTask = {
